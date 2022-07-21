cb21381e1ba4e093ee867cb550833ffde87d9cdb
忍者哈特利 <ks0dcongra@gmail.com>
2022-07-22 03:55:30 +0800

    feat: initialize api server

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
 const Handlebars = require('handlebars')
-const routes = require('./routes')
+const { pages } = require('./routes')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
   next()
 })
 
-app.use(routes)
+app.use(pages)
 app.listen(port, () => {
   console.info(`Example app listening on port ${port}!`)
 })
